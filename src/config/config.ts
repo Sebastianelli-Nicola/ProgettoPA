53d5c51943478c8a8112f3b245ad1ba12aae9b67
Ettore <ettorericci2002@gmail.com>
2025-06-26 11:38:04 +0200

    inizializzazione db

---
+++ src/config/config.ts
@@ -0,0 +1,11 @@
+const config = {
+    development: {
+        username: 'postgres',
+        password: 'postgres',
+        database: 'snap_auction',
+        host: '127.0.0.1',
+        dialect: 'postgres',
+        }
+};
+
+export = config;
