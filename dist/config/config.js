ce2f918c95a9b361d45ced7d49e697b194a7d3d5
Ettore <ettorericci2002@gmail.com>
2025-06-27 09:54:00 +0200

    le migrations funzionano correttamente

--- dist/config/config.js
+++ dist/config/config.js
@@ -13,14 +13,4 @@ module.exports = {
         dialect: 'postgres',
     },
 };
-/*const config = {
-    development: {
-        username: 'postgres',
-        password: 'postgres',
-        database: 'asta_snap',
-        host: '127.0.0.1',
-        dialect: 'postgres',
-        }
-};
 
-export = config;*/ 
